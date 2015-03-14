a0855ff1cc1b36e90655532933e7920165c73ff1
Nikolaj Schaldemose Reibke <nikolajsr12@gmail.com>
2015-03-14 15:46:18 +0100

    Added interface contracts for some files

---
+++ source/file_locator.r
@@ -0,0 +1,23 @@
+# File contains file location functions to find list of files.
+
+
+# Function Given a basepath "{basepath}" 
+# It will find the groupx/memberx/images
+# Given a specific dpi {100, 200, 300}
+#
+# Return a list of all paths to images
+
+
+
+
+
+# Function given:
+#  - Basepath
+#  - group
+#  - member
+#  - dpi {100, 200, 300}
+#
+# Return a list of the images for 
+#   that member in that group at given dpi.
+
+
---
+++ source/image_reader.r
@@ -0,0 +1,8 @@
+# Given:
+#
+# - List of Path to images
+# - Sigma value (default 1)
+# 
+# returns list of image (raw pixel values)
+
+
