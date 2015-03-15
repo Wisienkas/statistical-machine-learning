41982c8dfa0204a89a70ffc4505de855195c81d1
Thomas Rasmussen <git@thomasheine.dk>
2015-03-15 13:31:05 +0100

    Function to return all images are now working

--- source/file_locator.r
+++ source/file_locator.r
@@ -4,11 +4,39 @@
 # Function Given a basepath "{basepath}" 
 # It will find the groupx/memberx/images
 # Given a specific dpi {100, 200, 300}
-#
+
 # Return a list of all paths to images
-
-
-
+getImagePaths <- function(basePath, dpiArr) {  
+  
+  group_folders <- list.dirs(path = basePath)    
+  imagePaths <- c();
+  
+  #Run through all the folders
+  lapply(X = group_folders, FUN = function(dirPath){  
+    
+    #The folder must contain member before we want to look in it
+    if(length(grep("member", dirPath)) !=  0) {
+      
+      #All the files in the folder, which are PNG files
+      files <- list.files(dirPath, pattern = ".png", full.names = TRUE);
+      
+      #Check if the file contains the DPI we want
+      lapply(X = files, FUN = function(fileName){     
+        #See if the file contains the DPI
+        
+        #NOTE should it really be done in this way?!?!
+        lapply(X = dpiArr, FUN = function(dpi){
+          if(length(grep(dpi, fileName)) > 0) {                        
+            #Add the path to the list
+            imagePaths <<- c(imagePaths, fileName);      
+          }
+        })        
+      })        
+    }    
+  });  
+  
+  return (imagePaths);
+}
 
 
 # Function given:
@@ -21,3 +49,7 @@
 #   that member in that group at given dpi.
 
 
+##Testing
+dpiArrs <- c(100,200);
+patsh <- getImagePaths("E:\\SML_SVN\\trunk\\", dpiArrs);
+print(patsh);
