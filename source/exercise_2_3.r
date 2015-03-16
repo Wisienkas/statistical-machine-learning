e96933263dca533c5411abce509353dce1a1522b
Thomas Rasmussen <git@thomasheine.dk>
2015-03-16 22:40:35 +0100

    Work on exercise_2_3

--- source/exercise_2_3.r
+++ source/exercise_2_3.r
@@ -12,11 +12,14 @@ myData.classF <- classification(numbers = seq(0:9), times = 400)
 
 #All data
 allData.data.raw <- loadAllPeople(basePath = ressourcePath)
+
+#personer <- 3;
+#allData.data <- streamlineList(allData.data.raw)[1:(4000 * personer),];
 allData.data <- streamlineList(allData.data.raw);
 allData.classF <- rep(x = myData.classF, times = ( dim(allData.data)[1] / dim(myData.data)[1] ) )
 
 #Run KNN on the data
 text <- c();
-
 text <- knn.run(largeMatrix = allData.data, classes = allData.classF, k_arr = c(1,5,10), split = 0.9);
+#text <- knn.run(largeMatrix = myData.data, classes = myData.classF, k_arr = c(1,5,10), split = 0.9);
 print(text)
