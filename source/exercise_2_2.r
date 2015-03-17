2ed28643d36275d5ea9c1fd52d1ee67c949ba019
Thomas Rasmussen <git@thomasheine.dk>
2015-03-17 02:20:08 +0100

    Work on exercises

--- source/exercise_2_2.r
+++ source/exercise_2_2.r
@@ -5,15 +5,17 @@ source('normalization.r')
 source('knn_cross_validation.r')
 source('pca_optimization.r')
 
+
+
 best_cutoff <- 0.99;
 best_k <- 10;
 runs <- 10;
 
 #Path to images
-myData.path <- getSingleMemberImages('C:\\subversion\\', 'group6', 'member1');
+myData.path <- getSingleMemberImages('C:\\subversion\\', 'group6', 'member2');
 
 #The raw data
-myData.data.raw <- loadPersonsImageData(memberInfo = myData.path, sigma = 1, DPI = 100);
+myData.data.raw <- loadPersonsImageData(memberInfo = myData.path, sigma = 0, DPI = 100);
 
 myData.data <- streamlineList(myData.data.raw);
 
@@ -35,6 +37,13 @@ myData.pca_last.trunc <- pcaTruncate(data = myData.pca_last.norm, cutoff = best_
 #Run KNN with cross validation
 myData.pca_last.knn <- knnCrossValidation(myData.pca_last.trunc, runs = runs, k = best_k)
 
+myData.no_pca_no_norm.knn <- knnCrossValidation(myData.data, runs = runs, k = best_k);
+
+myData.no_norm_but_PCA.knn <- knnCrossValidation(myData.pca_first.trunc, runs = runs, k = best_k);
+
 #Results
+print(paste("No norm, PCA - ", "Mean: ", myData.no_norm_but_PCA.knn[[1]], " SD: ", myData.no_norm_but_PCA.knn[[2]], sep = ""));
+
+print(paste("No norm, no PCA - ", "Mean: ", myData.no_pca_no_norm.knn[[1]], " SD: ", myData.no_pca_no_norm.knn[[2]], sep = ""));
 print(paste("PCA first - ", "Mean: ", myData.pca_first.knn[[1]], " SD: ", myData.pca_first.knn[[2]], sep = ""));
 print(paste("PCA after - ", "Mean: ", myData.pca_last.knn[[1]], " SD: ", myData.pca_last.knn[[2]], sep = ""));
--- source/exercise_2_3.r
+++ source/exercise_2_3.r
@@ -6,20 +6,21 @@ ressourcePath <- 'C:\\subversion\\'
 
 #My data
 myData.path <- getSingleMemberImages(basePath = ressourcePath, 'group6', 'member1');
-myData.data.raw <- loadPersonsImageData(memberInfo = myData.path, sigma = 1, DPI = 100)
+myData.data.raw <- loadPersonsImageData(memberInfo = myData.path, sigma = 0, DPI = 100)
 myData.data <- streamlineList(myData.data.raw)
 myData.classF <- classification(numbers = seq(0:9), times = 400)
 
 #All data
 allData.data.raw <- loadAllPeople(basePath = ressourcePath)
 
-#personer <- 3;
-#allData.data <- streamlineList(allData.data.raw)[1:(4000 * personer),];
-allData.data <- streamlineList(allData.data.raw);
+personer <- 14;
+allData.data <- streamlineList(allData.data.raw)[1:(4000 * personer),];
+#allData.data <- streamlineList(allData.data.raw);
 allData.classF <- rep(x = myData.classF, times = ( dim(allData.data)[1] / dim(myData.data)[1] ) )
 
 #Run KNN on the data
 text <- c();
-text <- knn.run(largeMatrix = allData.data, classes = allData.classF, k_arr = c(1,5,10), split = 0.9);
+#text <- knn.run(largeMatrix = allData.data, classes = allData.classF, k_arr = c(1,5,10), split = 0.9);
+text <- knn.run_split(largeMatrix = allData.data, classes = allData.classF, k_arr = c(1,5,10), split = 13 / 14, chunkSize = 4000)
 #text <- knn.run(largeMatrix = myData.data, classes = myData.classF, k_arr = c(1,5,10), split = 0.9);
 print(text)
