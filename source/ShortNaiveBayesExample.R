a35a150177a31306bf4d6a89ba3a35bc840fc8fc
Nikolaj Reibke <nikolajsr12@gmail.com>
2015-03-22 03:51:16 +0100

    Added some examples with categorical Naives bayes

---
+++ source/ShortNaiveBayesExample.R
@@ -0,0 +1,21 @@
+install.packages("klaR")
+install.packages("caret")
+
+library(klaR)
+library(e1071)
+library("caret")
+
+# Showing Example with Iris
+x <- iris[, -5]
+y <- iris$Species
+
+model = train(x = x, 
+              y = y,
+              method = 'nb',
+              trControl=trainControl(method='cv',
+                                     number=10)
+              )
+
+myPredict <- predict(model$finalMode, x)
+
+crossTable <- table(myPredict$class, y)
